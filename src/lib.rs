be13ab67836012e8368f506564438f76733eacf4
Kevin R. Thornton <krthornt@uci.edu>
2026-04-16 15:04:14 -0700

    chore: various details for publishing the crate

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,44 @@
 //! By way of example:
 //!
 //!```rust
-#![doc = include_str!("../python_example/maketrees/src/lib.rs")]
+//! use pyo3::prelude::*;
+//! 
+//! /// A Python module implemented in Rust.
+//! #[pymodule]
+//! // The setup is designed for a mixed rust/python
+//! // project. We compile the rust side to the following name,
+//! // with the intent that __init__.py imports this to bring
+//! // the public API into scope.
+//! #[pyo3(name = "_maketrees")]
+//! mod maketrees {
+//!     use pyo3::prelude::*;
+//!     #[pyfunction]
+//!     fn maketrees(py: Python<'_>) -> PyResult<Py<PyAny>> {
+//!         let mut holder = tskit2tskit::SharedTableCollection::new(py, 100.0).unwrap();
+//!         // Release the gil to work only on the rust side of the data,
+//!         // potentially allowing other Python threads to run.
+//!         py.detach(|| -> Result<(), PyErr> {
+//!             // SAFETY: the code below is safe if tskit-rust and tskit-python
+//!             // are built around the same layout for `tsk_table_collection_t`.
+//!             Ok(unsafe {
+//!                 holder.with_mut_tables(|t| -> Result<(), tskit2tskit::Error> {
+//!                     // Everything in this block is the standard tskit rust API.
+//!                     // Note that the use if ? will convert and TskitError into
+//!                     // a tskit2tskit::Error.
+//!                     let parent = t.add_node(0, 1.0, -1, -1)?;
+//!                     let c0 = t.add_node(tskit::NodeFlags::IS_SAMPLE, 0.0, -1, -1)?;
+//!                     let c1 = t.add_node(tskit::NodeFlags::IS_SAMPLE, 0.0, -1, -1)?;
+//!                     t.add_edge(0., 100., parent, c0)?;
+//!                     t.add_edge(0., 100., parent, c1)?;
+//!                     Ok(())
+//!                 })
+//!             }?) // The ? here will convert tskit2tskit::Error into a PyErr
+//!         })?;
+//!         // Returns Python tskit.TreeSequence
+//!         // Again, error types will propagate into PyErr as needed.
+//!         Ok(holder.into_python_tree_sequence(py)?)
+//!     }
+//! }
 //!```
 //! The above code cleanly maps from [`tskit::TskitError`] to [`Error`] to [`PyErr`]
 //! by means of [`std::convert::From`].
