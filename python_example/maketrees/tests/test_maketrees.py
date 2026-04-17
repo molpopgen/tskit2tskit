422a94ea549368a0216406e44094f24405fbeef4
Kevin R. Thornton <krthornt@uci.edu>
2026-04-17 13:24:39 -0700

    test: extra asserts plus a tidy up

--- python_example/maketrees/tests/test_maketrees.py
+++ python_example/maketrees/tests/test_maketrees.py
@@ -12,7 +12,6 @@ def test_return_value():
     assert ts.num_nodes == 3
     assert ts.num_edges == 2
     times = ts.tables.nodes.time
-    print(times)
     assert len([i for i in times if i == 0.0]) == 2
     assert len([i for i in times if i == 1.0]) == 1
 
